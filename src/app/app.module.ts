984bca4522b8af31841832f5a546956a60fbd8cf
Xewol <bartekdeluga0702@gmail.com>
2023-11-27 22:11:21 +0100

    feat: lab-a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, provideRouter } from '@angular/router';
+import routeConfig from './routes';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, HomeComponent, RouterModule],
+  providers: [provideRouter(routeConfig)],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
